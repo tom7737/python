337a0f43d92be629436e0ecf425cc03d1628b4bb
tom <tom7737@sina.com>
2016-12-16 14:30:09 +0800

    day8

--- awesome-python-webapp/www/wsgiapp.py
+++ awesome-python-webapp/www/wsgiapp.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from config import configs
+from ms import db
+from ms.web import WSGIApplication, Jinja2TemplateEngine
 from www.ms.db import engine
+import os
+import time
+import urls
+from xmlrpclib import datetime
 
 __author__ = 'Michael Liao'
 
@@ -10,12 +17,8 @@ A WSGI application entry.
 
 import logging; logging.basicConfig(level=logging.INFO)
 
-import os
 
-from ms import db
-from ms.web import WSGIApplication, Jinja2TemplateEngine
 
-from config import configs
 
 # init db:
 db.create_engine(**configs.db)
@@ -23,11 +26,28 @@ db.create_engine(**configs.db)
 # init wsgi app:
 wsgi = WSGIApplication(os.path.dirname(os.path.abspath(__file__)))
 
+# 定义datetime_filter，输入是t，输出是unicode字符串:
+def datetime_filter(t):
+    logging.warn(time.time())
+    delta = int(time.time() - t)
+    if delta < 60:
+        return u'1分钟前'
+    if delta < 3600:
+        return u'%s分钟前' % (delta // 60)
+    if delta < 86400:
+        return u'%s小时前' % (delta // 3600)
+    if delta < 604800:
+        return u'%s天前' % (delta // 86400)
+    dt = datetime.fromtimestamp(t)
+    return u'%s年%s月%s日' % (dt.year, dt.month, dt.day)
+
 template_engine = Jinja2TemplateEngine(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates'))
+
+# 把filter添加到jinjia2，filter名称为datetime，filter本身是一个函数对象:
+template_engine.add_filter('datetime', datetime_filter)
 
 wsgi.template_engine = template_engine
 
-import urls
 
 wsgi.add_module(urls)
 
