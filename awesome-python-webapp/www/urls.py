be6f3de54edf2ebb7893e65cc8adfcf3a1e5103a
tom <tom7737@sina.com>
2016-12-16 16:58:12 +0800

    day9

--- awesome-python-webapp/www/urls.py
+++ awesome-python-webapp/www/urls.py
@@ -5,7 +5,7 @@ __author__ = 'Michael Liao'
 
 import os, re, time, base64, hashlib, logging
 
-from ms.web import get, post, ctx, view, interceptor, seeother, notfound
+from ms.web import get, post, ctx, view, interceptor, seeother, notfound,api
 
 from models import User, Blog, Comment
 
@@ -15,3 +15,11 @@ def index():
     blogs = Blog.find_all()
     user = User.find_first('where email=?', 'admin@example.com')
     return dict(blogs=blogs, user=user)
+
+@api
+@get('/api/users')
+def api_get_users():
+    users = User.find_by('order by created_at desc')
+    for u in users:
+        u.password = '******'
+    return dict(users=users)
